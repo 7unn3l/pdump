17f97f8e60297c09c7a541389f3933d2520b7221
7unn3l <maxrademacher02@gmail.com>
2022-06-20 15:30:30 +0200

    now able to dump simple asm generated bin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use nix::libc::execve;
 use nix::unistd::{fork,Pid, ForkResult};
 use nix::sys::ptrace;
+use core::panic;
 use std::env;
 use std::ffi::CString;
+use std::io::Write;
 use nix::sys::wait::waitpid;
 use nix::sys::personality;
 use nix::sys::personality::Persona;
+use std::fs::File;
+use std::path::Path;
 
 enum Endianess{
     Big,
@@ -46,7 +50,8 @@ fn child_setup(elf:&str){
     return;
 }
 
-fn search_elf_header(pid: Pid,entry_rip: u64){
+fn search_elf_header(pid: Pid,entry_rip: u64,start:u64) -> Option<(u64,u64)>{
+    println!("conducting search for ELF header. Starting at 0x{:x?}...",start);
     /*
     search until ELF header is found. Validate via
     signature and entrypoint
@@ -54,35 +59,71 @@ fn search_elf_header(pid: Pid,entry_rip: u64){
 
     let endianess =  get_endianess();
 
-    let mut addr: u64 = 0;
+    let mut addr: u64 = start;
     loop{
+        /*if addr % (8*500000) == 0{
+            println!("at 0x{:x?}",addr);
+        }*/
+
         let result: i64 = match ptrace::read(pid,addr as *mut _){
             Ok(val) => val,
             Err(_) => {
                 // we might not be ata valid addr, skip this.
-                addr += 8;
+                addr = match addr.checked_add(8){
+                    Some(x) => x,
+                    None => break // we've gone through all addresses
+                };
                 continue;
             }
         };
 
-
-        if result != 0{
-            let result = match endianess{
-                Endianess::Big => result.to_be_bytes(),
-                Endianess::Little => result.to_le_bytes()
-            };
+        let result = match endianess{
+            Endianess::Big => result.to_be_bytes(),
+            Endianess::Little => result.to_le_bytes()
+        };
             
-            // todo: iterators, closures
-            match result.windows(4).position(|window| window == MAGIC){
-                Some(index) => println!("found at index {}, total addr is {}",index,addr+index as u64),
-                None => {}
-            }
+        // todo: iterators, closures
+        match result.windows(4).position(|window| window == MAGIC){
+            Some(index) => {
+                let index = index as u64;
+                let elf_start = index as u64+addr+index;
+                
+                println!("found a possible ELF header at memory 0x{:x?}",elf_start);
+                
+                let parsed_entry: i64 = ptrace::read(pid,(elf_start+24) as *mut _).unwrap();
 
-            break;
+                println!("parsed entry: 0x{:x?}, rip = 0x{:x?}",parsed_entry,entry_rip);
+
+                if parsed_entry != entry_rip as i64{
+
+                    /* 
+                    We verified the ELF header. Now calculate size of the binary
+                    in memory via section header table offset.
+                    */
+                
+                    let offset_sect_headers: u64 = ptrace::read(pid,(elf_start+40) as *mut _).unwrap() as u64;
+                    let size_sect_header = ptrace::read(pid,(elf_start+58) as *mut _).unwrap() as u16;
+                    let num_sect_headers = ptrace::read(pid,(elf_start+60) as *mut _).unwrap() as u16;
+
+                    println!("section header size: {}",size_sect_header);
+
+                    let size_sect_headers = size_sect_header*num_sect_headers;
+                    let total_bin_size: u64 = offset_sect_headers+size_sect_headers as u64;
+
+                    println!("header valid. Binary size = 0x{:x?} bytes",total_bin_size);
+
+                    return Some((elf_start,elf_start+total_bin_size));
+                }
+
+            },
+            None => {}
         }
-        
-        addr += 8;
+        addr = match addr.checked_add(8){
+            Some(x) => x,
+            None => break // we've gone through all addresses
+        };
     }
+    return None;
 
 }
 
@@ -95,8 +136,38 @@ fn dump_child(pid:Pid){
     }
 
     let current_rip: u64 = ptrace::getregs(pid).expect("could not get current rip value").rip;
+
+    println!("current rip: 0x{:x?}",current_rip);
     
-    search_elf_header(pid,current_rip);
+
+    // Use start to sepcify a starting point for memory search
+    let (elf_start,elf_end) = match search_elf_header(pid,current_rip,0x555555550000){
+        Some(x) => x,
+        None => {
+            println!("could not find ELF header in memory");
+            std::process::exit(1);},
+    };
+
+    println!("dumping ELF data beginning from memory offset 0x{:x?} to 0x{:x?}...",elf_start,elf_end);
+    let required_reads = (elf_end-elf_start) / 8;
+
+    let mut file = match File::create(&Path::new("./dump")){
+        Err(e) => panic!("could not create file ./dump: {}",e),
+        Ok(file) => file,
+    };
+
+    for n in 0..required_reads{
+        let addr = elf_start+n*8;
+
+        let buf = match ptrace::read(pid,addr as *mut _){
+            Ok(x) => x.to_le_bytes(),
+            Err(_) => {continue}
+        };
+
+        file.write(&buf).expect("could not write to ./dump");
+    }
+
+    println!("finished. see ./dump");
 
 }
 
